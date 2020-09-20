bd1f2d4f3d387fe2cde67f42277fd68c719650c6
Cornelius Ashley-Osuzoka <korneliosyaovi@gmail.com>
2020-09-20 19:08:41 +0100

    updated bugs in test files

--- test/rave.settlements.test.js
+++ test/rave.settlements.test.js
@@ -1,45 +1,41 @@
-// require('dotenv').config({
-//     path: '../.env'
-// });
-
-// const settlement = require('../lib/rave.settlements');
-// const base = require('../lib/rave.base');
-// const Promise = require('bluebird');
-// const mocha = require('mocha');
-// const chai = require('chai');
-// const expect = chai.expect;
-// const chaiAsPromised = require('chai-as-promised');
+var settlement = require('../lib/rave.settlements');
+var base = require('../lib/rave.base');
+var Promise = require('bluebird');
+var mocha = require('mocha');
+var chai = require('chai');
+var expect = chai.expect;
+var chaiAsPromised = require('chai-as-promised');
+var dotenv = require('dotenv').config();
 
 
+chai.use(chaiAsPromised);
 
-// chai.use(chaiAsPromised);
-
-// describe("#Rave Settlements", function () {
+describe("#Rave Settlements", function () {
 
 
-//     const public_key = process.env.PUBLIC_KEY;
-//     const secret_key = process.env.SECRET_KEY;
-//     const ravebase = new base(public_key, secret_key);
-//     const settlementInstance = new settlement(ravebase);
+    const public_key = process.env.PUBLIC_KEY;
+    const secret_key = process.env.SECRET_KEY;
+    const ravebase = new base(public_key, secret_key);
+    const settlementInstance = new settlement(ravebase);
 
-//     it("should return all settlements ", async function () {
-//         this.timeout(10000);
+    it("should return all settlements ", async function () {
+        this.timeout(10000);
 
        
-//         var resp = await settlementInstance.fetch_all();
-//         return expect(resp).to.have.property('message')
-//     });
+        var resp = await settlementInstance.fetch_all();
+        return expect(resp).to.have.property('message')
+    });
 
-//     it("should return list of bank branches ", async function () {
-//         this.timeout(10000);
+    // it("should return list of bank branches ", async function () {
+    //     this.timeout(10000);
 
-//         var payload = {
-//             "currency": "NGN",
-//             "amount": 4000,
-//             "reference":"RVEBLS-F81CEEEE8218-73362"
-//         }
-//         var resp = await settlementInstance.update(payload);
-//         return expect(resp).to.have.property('data')
-//     });
+    //     var payload = {
+    //         "currency": "NGN",
+    //         "amount": 4000,
+    //         "reference":"RVEBLS-F81CEEEE8218-73362"
+    //     }
+    //     var resp = await settlementInstance.update(payload);
+    //     return expect(resp).to.have.property('data')
+    // });
 
-// });
+});
