c42c3d2ff115b408a06e0d61d49f8c34f6db4065
Ethical Ralph <rakinola90@gmail.com>
2020-10-02 14:06:44 +0100

    feat: more assertions for bank test

--- test/rave.bank.test.js
+++ test/rave.bank.test.js
@@ -7,35 +7,36 @@ var expect = chai.expect;
 var chaiAsPromised = require('chai-as-promised');
 var dotenv = require('dotenv').config();
 
-
-
 chai.use(chaiAsPromised);
 
-describe("#Rave Bank", function () {
+describe('#Rave Bank', function () {
+  const public_key = process.env.PUBLIC_KEY;
+  const secret_key = process.env.SECRET_KEY;
+  const ravebase = new base(public_key, secret_key);
+  var banksInstance = new banks(ravebase);
 
-    const public_key = process.env.PUBLIC_KEY;
-    const secret_key = process.env.SECRET_KEY;
-    const ravebase = new base(public_key, secret_key);
-    var banksInstance = new banks(ravebase);
+  it('should return list of banks in NG ', async function () {
+    this.timeout(10000);
 
-    it("should return list of banks in NG ", async function () {
-        this.timeout(10000);
+    var payload = {
+      country: 'NG',
+    };
+    var resp = await banksInstance.country(payload);
+    expect(resp).to.have.property('data');
+    expect(resp.status).to.equals('success');
+    expect(resp.data).to.be.an('array');
+    return;
+  });
 
-        var payload = {
-            "country": "NG"
-        }
-        var resp = await banksInstance.country(payload);
-        return expect(resp).to.have.property('data')
-    });
-
-    it("should return list of bank branches ", async function () {
-        this.timeout(10000);
-
-        var payload = {
-            "id": "280"
-        }
-        var resp = await banksInstance.branches(payload);
-        return expect(resp).to.have.property('data')
-    });
-
+  it('should return list of bank branches ', async function () {
+    this.timeout(10000);
+    var payload = {
+      id: '280',
+    };
+    var resp = await banksInstance.branches(payload);
+    expect(resp).to.have.property('data');
+    expect(resp.status).to.equals('success');
+    expect(resp.data).to.be.an('array');
+    return;
+  });
 });
