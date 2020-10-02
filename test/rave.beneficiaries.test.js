1ece50f6be4c19d7303c5da1a2f29219755b2c19
Ethical Ralph <rakinola90@gmail.com>
2020-10-02 14:20:06 +0100

    feat: more assertions and improvement for beneficiaries test

--- test/rave.beneficiaries.test.js
+++ test/rave.beneficiaries.test.js
@@ -7,48 +7,51 @@ var expect = chai.expect;
 var chaiAsPromised = require('chai-as-promised');
 var dotenv = require('dotenv').config();
 
-
-
 chai.use(chaiAsPromised);
 
-describe("#Rave Beneficiaries", function () {
+describe('#Rave Beneficiaries', function () {
+  const public_key = process.env.PUBLIC_KEY;
+  const secret_key = process.env.SECRET_KEY;
+  const ravebase = new base(public_key, secret_key);
+  var beneficiariesInstance = new beneficiaries(ravebase);
+  let beneficiaryId;
 
+  it('should create Beneficiary', async function () {
+    this.timeout(10000);
 
-    const public_key = process.env.PUBLIC_KEY;
-    const secret_key = process.env.SECRET_KEY;
-    const ravebase = new base(public_key, secret_key);
-    var beneficiariesInstance = new beneficiaries(ravebase);
+    var payload = {
+      account_number: '0690000034',
+      account_bank: '044',
+    };
+    var resp = await beneficiariesInstance.create(payload);
+    beneficiaryId = resp.data.id;
+    expect(resp.status).to.equal('success');
+    expect(resp).to.have.property('data');
+    return;
+  });
 
-    it("should create Beneficiary", async function () {
-        this.timeout(10000);
+  it('should return a single beneficiary ', async function () {
+    this.timeout(10000);
 
-        var payload = {
-            "account_number": "0690000034",
-            "account_bank":"044"
-        }
-        var resp = await beneficiariesInstance.create(payload);
-        return expect(resp).to.have.property('data')
-  
-    });
+    var payload = {
+      id: beneficiaryId,
+    };
+    var resp = await beneficiariesInstance.fetch(payload);
+    expect(resp.status).to.equal('success');
+    expect(resp).to.have.property('data');
+    expect(resp.data).to.not.be.empty;
+    return;
+  });
 
-    it("should return a single beneficiary ", async function () {
-        this.timeout(10000);
+  it('should return delete message', async function () {
+    this.timeout(10000);
 
-        var payload = {
-           "id":"3644"
-        }
-        var resp = await beneficiariesInstance.fetch(payload);
-        return expect(resp).to.have.property('data')
-    });
-
-    it("should return delete message", async function () {
-        this.timeout(10000);
-
-        var payload = {
-           "id":"3644"
-        }
-        var resp = await beneficiariesInstance.delete(payload);
-        return expect(resp).to.have.property('message')
-    });
-
+    var payload = {
+      id: beneficiaryId,
+    };
+    var resp = await beneficiariesInstance.delete(payload);
+    expect(resp.status).to.equal('success');
+    expect(resp).to.have.property('message');
+    return;
+  });
 });
