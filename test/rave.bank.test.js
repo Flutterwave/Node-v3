f85e29ee1be15ff5bfb67fabb30d525ccf377125
Cornelius Ashley <cornelius@flutterwavego.com>
2022-08-01 16:34:24 +0100

    bugfixes for issues #44 and #51

--- test/rave.bank.test.js
+++ test/rave.bank.test.js
@@ -1,4 +1,5 @@
 var banks = require('../lib/rave.banks');
+var payout = require('../lib/rave.transfers');
 var base = require('../lib/rave.base');
 var Promise = require('bluebird');
 var mocha = require('mocha');
@@ -17,6 +18,40 @@ describe("#Rave Bank", function () {
     const secret_key = process.env.SECRET_KEY;
     const ravebase = new base(public_key, secret_key);
     var banksInstance = new banks(ravebase);
+    var transferInstance = new payout(ravebase);
+
+    it("should create a XAF transfer ", async function () {
+        this.timeout(15000);
+
+        var payload = {
+            "account_bank": "FMM",
+            "account_number": "250700000000",
+            "amount": 50,
+            "narration": "Node XAF Sample Transfer",
+            "currency": "XAF",
+            "reference": "Node_1_PMCKDU_1",
+            "beneficiary_name": "Flutterwave Developers"
+          }
+        var resp = await transferInstance.initiate(payload);
+        return expect(resp).to.have.property('data')
+    });
+
+    it("should create a NGN transfer ", async function () {
+        this.timeout(15000);
+
+        var payload = {
+            "account_bank": "044",
+            "account_number": "0690000040",
+            "amount": 5500,
+            "narration": "Node NGN Sample Transfer",
+            "currency": "NGN",
+            "reference": "Node_2_PMCKDU_1",
+            "callback_url": "https://www.flutterwave.com/ng/",
+            "debit_currency": "NGN"
+        }
+        var resp = await transferInstance.initiate(payload);
+        return expect(resp).to.have.property('data')
+    });
 
     it("should return list of banks in NG ", async function () {
         this.timeout(10000);
--- test/rave.subscriptions.test.js
+++ test/rave.subscriptions.test.js
@@ -6,6 +6,7 @@ var chai = require('chai');
 var expect = chai.expect;
 var chaiAsPromised = require('chai-as-promised');
 const Subscriptions = require('../lib/rave.subscriptions');
+const Plan = require('../lib/rave.payment_plan');
 var dotenv = require('dotenv').config();
 
 
@@ -19,8 +20,19 @@ describe("#Rave Subscriptions", function () {
     const secret_key = process.env.SECRET_KEY;
     const ravebase = new base(public_key, secret_key);
     var subscriptionInstance = new Subscriptions(ravebase);
+    var planInstance = new Plan (ravebase);
 
-    
+    it("should create a plan without duration ", async function () {
+        this.timeout(10000);
+
+        var payload = {
+            "amount":5000,
+            "name":"Node test plan",
+            "interval":"monthly"
+        }
+        var resp = await planInstance.create(payload);
+        return expect(resp).to.have.property('data')
+    });
 
     it("should return a single subscription ", async function () {
         this.timeout(10000);
