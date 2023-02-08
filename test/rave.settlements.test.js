cf0e8ac75aa1a7332a11c188c894dfa7dbd3fd19
Cornelius Ashley <cornelius@flutterwavego.com>
2023-02-08 11:08:45 +0100

    removed bill tests

--- test/rave.settlements.test.js
+++ test/rave.settlements.test.js
@@ -1,36 +1,36 @@
-var settlement = require('../lib/rave.settlements');
-var base = require('../lib/rave.base');
-var Promise = require('bluebird');
-var mocha = require('mocha');
-var chai = require('chai');
-var expect = chai.expect;
-var chaiAsPromised = require('chai-as-promised');
-var dotenv = require('dotenv').config();
+// var settlement = require('../lib/rave.settlements');
+// var base = require('../lib/rave.base');
+// var Promise = require('bluebird');
+// var mocha = require('mocha');
+// var chai = require('chai');
+// var expect = chai.expect;
+// var chaiAsPromised = require('chai-as-promised');
+// var dotenv = require('dotenv').config();
 
-chai.use(chaiAsPromised);
+// chai.use(chaiAsPromised);
 
-describe('#Rave Settlements', function () {
-  const public_key = process.env.PUBLIC_KEY;
-  const secret_key = process.env.SECRET_KEY;
-  const ravebase = new base(public_key, secret_key);
-  const settlementInstance = new settlement(ravebase);
+// describe('#Rave Settlements', function () {
+//   const public_key = process.env.PUBLIC_KEY;
+//   const secret_key = process.env.SECRET_KEY;
+//   const ravebase = new base(public_key, secret_key);
+//   const settlementInstance = new settlement(ravebase);
 
-  it('should return all settlements ', async function () {
-    this.timeout(10000);
+//   it('should return all settlements ', async function () {
+//     this.timeout(10000);
 
-    var resp = await settlementInstance.fetch_all();
-    return expect(resp).to.have.property('message');
-  });
+//     var resp = await settlementInstance.fetch_all();
+//     return expect(resp).to.have.property('message');
+//   });
 
-  // it("should return list of bank branches ", async function () {
-  //     this.timeout(10000);
+//   // it("should return list of bank branches ", async function () {
+//   //     this.timeout(10000);
 
-  //     var payload = {
-  //         "currency": "NGN",
-  //         "amount": 4000,
-  //         "reference":"RVEBLS-F81CEEEE8218-73362"
-  //     }
-  //     var resp = await settlementInstance.update(payload);
-  //     return expect(resp).to.have.property('data')
-  // });
-});
+//   //     var payload = {
+//   //         "currency": "NGN",
+//   //         "amount": 4000,
+//   //         "reference":"RVEBLS-F81CEEEE8218-73362"
+//   //     }
+//   //     var resp = await settlementInstance.update(payload);
+//   //     return expect(resp).to.have.property('data')
+//   // });
+// });
