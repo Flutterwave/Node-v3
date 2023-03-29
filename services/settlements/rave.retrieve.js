e44837baa26cb31f97fddf62de343c1e7d52b803
Cornelius Ashley <cornelius@flutterwavego.com>
2023-03-29 16:36:18 +0100

    bugfixes on new schemas

--- services/settlements/rave.retrieve.js
+++ services/settlements/rave.retrieve.js
@@ -1,9 +1,9 @@
 const { logger } = require('../../utils/logger');
 const { validator } = require('../../utils/validator');
-const { fetchSchema } = require('../schema/base');
+const { listSchema } = require('../schema/base');
 
 async function service(data, _rave) {
-  validator(fetchSchema, data);
+  validator(listSchema, data);
   data.method = 'GET';
   const { body: response } = await _rave.request(
     `/v3/settlements/${data.id}`,
--- services/virtual-account/rave.retrieve.bulk.js
+++ services/virtual-account/rave.retrieve.bulk.js
@@ -1,8 +1,11 @@
+const { logger } = require('../../utils/logger');
+const { validator } = require('../../utils/validator');
 const { fetchBulkAccountSchema } = require('../schema/auxillary');
 
 async function service(data, _rave) {
   validator(fetchBulkAccountSchema, data);
   data.method = 'GET';
+  data.excludeQuery = true;
   const { body: response } = await _rave.request(
     `v3/bulk-virtual-account-numbers/${data.batch_id}`,
     data,
